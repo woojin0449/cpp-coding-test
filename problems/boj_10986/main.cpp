3c3e919d8d9afb7e0ef3cb6893a64f28b791d374
woojin kim <jxt6380@gmail.com>
2025-09-26 14:44:30 +0000

    09-26

--- problems/boj_10986/main.cpp
+++ problems/boj_10986/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
+int combin_2(int a){
+    return a*(a-1)/2;
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n,m;
-    
+    int n,m,total=0;
+    cin>>n>>m;
+    vector<int>A(n+1,0);
+    vector<long long>D(n+1,0);
+    vector <long long>index(m+1,0); // if m=3, then 0 1 2 because modular
+    for(int i=1;i<=n;i++){
+        cin>>A[i];
+    }
+    for(int i=1;i<=n;i++){ //합 수열
+        D[i]=D[i-1]+A[i];
+    }
+    for(int i=1;i<=n;i++){ //합쳐도 될거 같은데..
+        D[i]=D[i]%m;
+    }
+    for(int i=1;i<=n;i++){
+        if(D[i]==0){
+            total++;
+        }
+        index[D[i]]++;
+        
+    }
+    for(int i=0;i<m;i++){
+        if(index[i]>1){
+            total += combin_2(index[i]);
+        }
+        
+    }
+    cout<<total<<"\n";
     return 0;
 }
