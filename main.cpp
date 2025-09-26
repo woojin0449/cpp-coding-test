a31fcb576f1301683d88327e85cb8ff52e3421e6
woojin kim <jxt6380@gmail.com>
2025-09-26 13:33:03 +0000

    iostream

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cout << "Hello, Gitpod C++" << endl;
+    
     return 0;
 }
